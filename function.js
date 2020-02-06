ef961d8a31e557fe97df839dad7bfc95b555531d
Varsine <varsulmozg@gmail.com>
2020-02-06 20:22:22 +0400

    add first fayles

---
+++ function.js
@@ -0,0 +1,42 @@
+let list = document.querySelector('#list'),
+    form = document.querySelector('form'),
+    item = document.querySelector('#item');
+
+// set storge
+
+function addStorge() {
+    window.localStorage.myitems = list.innerHTML;
+}
+
+// add input value
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    list.innerHTML += '<li>' + item.value + '</li>';
+    addStorge();
+    item.value = "";
+})
+
+// remove input value
+
+list.addEventListener('click', function(event) {
+    let t = event.target;
+    if (t.classList.contains('checked')) {
+        t.parentNode.removeChild(t);
+    } else {
+        t.classList.add('checked');
+    }
+    addStorge();
+})
+
+// get storge
+
+function getStorge() {
+    var storedValues = window.localStorage.myitems;
+    if (!storedValues) {
+        list.innerHTML = '<li>Play gitar</li>';
+    } else {
+        list.innerHTML = storedValues;
+    }
+}
+getStorge();
